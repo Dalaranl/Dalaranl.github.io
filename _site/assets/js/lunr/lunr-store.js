1e9e3dab322fda66f1fbefc8085a319bdc55f33c
Gichang Sung <minjiwr123@gmail.com>
2022-04-22 23:22:58 +0900

    구글 서치 등록

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "테스트 포스팅",
-        "excerpt":"첫 테스트 포스팅   즐겁게 꾸며보자구      동일 경로 테스트    프리티어 테스트   걍 vscode로 하자 미리보기만 어케하면 됨   vs로해도 실시간으로 보이면 개꿀 새로고침 해야 보이네 ㅎㅎ   두번 띄어야 줄바꿈   아직 기본셋팅이 안끝났다.   // 코드 테스트 export defalut function Test() {     const test = 0;          if (test === 0) return;  }  ","categories": ["coding"],
+        "excerpt":"첫 테스트 포스팅 즐겁게 꾸며보자구 목차 테스트 동일 경로 테스트 프리티어 테스트 목차 테스트 걍 vscode로 하자 미리보기만 어케하면 됨 vs로해도 실시간으로 보이면 개꿀 새로고침 해야 보이네 ㅎㅎ 두번 띄어야 줄바꿈 아직 기본셋팅이 안끝났다. // 코드 테스트 export defalut function Test() { const test = 0; if (test === 0)...","categories": ["coding"],
         "tags": ["blog","jekyll"],
         "url": "/coding/first/",
         "teaser": null
