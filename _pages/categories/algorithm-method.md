8b089252a18625699e07b3b9b93bfcd0504c0ade
Gichang Sung <minjiwr123@gmail.com>
2022-05-02 23:54:15 +0900

    Post: 2022.05.02

---
+++ _pages/categories/algorithm-method.md
@@ -0,0 +1,12 @@
+---
+title: "algorithm method"
+layout: archive
+permalink: categories/method
+author_profile: true
+sidebar:
+    nav: "docs"
+---
+
+
+{% assign posts = site.categories.method %}
+{% for post in posts %} {% include archive-single.html type=page.entries_layout %} {% endfor %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["data-*","css"],
         "url": "/css/Custom-Data-Attributes/",
         "teaser": "/assets//images/2022-04-25/%EB%8B%A4%ED%81%AC%EB%AA%A8%EB%93%9C.gif"
+      },{
+        "title": "typeof와 Data type",
+        "excerpt":"JavaScript에는 ‘typeof’라는 연산자가 있다. 이는 자료형을 문자열로 반환해주는 연산자로 코드를 만들 때 종종 사용하게 된다. 하지만 이 연산자를 쓸 때 가끔 생각과는 다른 결과가 종종 나오기도 한다. 이는 Data type과 연관이 있는데 어떠한 경우에 이런 현상이 나타나는지 왜 이런 현상이 발생하는지에 대해 포스팅 하려한다. typeof typeof 연산자는 피연산자의 평가 전...","categories": ["javascript"],
+        "tags": ["JavaScript","연산자","typeof","Data type","원시값","Primitive type","Reference type"],
+        "url": "/javascript/typeof/",
+        "teaser": "/assets/thumbnail/javascript.png"
       }]
